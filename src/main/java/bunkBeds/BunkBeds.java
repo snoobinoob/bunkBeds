afddbbf30cd50507d2665d2af99738d0aaf70a60
Snoobinoob <cheeseycheese99@gmail.com>
2023-08-26 21:17:57 -0400

    Initial commit

---
+++ src/main/java/bunkBeds/BunkBeds.java
@@ -0,0 +1,11 @@
+package bunkBeds;
+
+import necesse.engine.modLoader.annotations.ModEntry;
+
+@ModEntry
+public class BunkBeds {
+
+    public void init() {
+        System.out.println("Hello world from Bunk Beds!");
+    }
+}
