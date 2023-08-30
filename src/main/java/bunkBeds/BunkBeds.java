439e1e329345540843ea3e5c7cc2c37b3232f95f
Snoobinoob <cheeseycheese99@gmail.com>
2023-08-29 20:29:35 -0400

    Functionality: Remove shared room happiness penalty

--- src/main/java/bunkBeds/BunkBeds.java
+++ src/main/java/bunkBeds/BunkBeds.java
@@ -1,11 +1,17 @@
 package bunkBeds;
 
 import necesse.engine.modLoader.annotations.ModEntry;
+import necesse.engine.modLoader.annotations.ModMethodPatch;
+import necesse.level.maps.levelData.settlementData.SettlementRoom;
+import net.bytebuddy.asm.Advice;
 
 @ModEntry
 public class BunkBeds {
-
-    public void init() {
-        System.out.println("Hello world from Bunk Beds!");
+    @ModMethodPatch(target = SettlementRoom.class, name = "getOccupiedBeds", arguments = {})
+    public static class GetOccupiedBedsPatch {
+        @Advice.OnMethodExit
+        public static void onExit(@Advice.Return(readOnly = false) int occupiedBeds) {
+            occupiedBeds = Math.min(occupiedBeds, 1);
+        }
     }
 }
