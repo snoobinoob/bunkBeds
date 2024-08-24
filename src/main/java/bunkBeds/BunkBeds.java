9d53d6d9da6eb84dbb849e6ad3113c4635de157c
Snoobinoob <cheeseycheese99@gmail.com>
2024-08-24 10:30:19 -0400

    Update for game version 0.26.1

--- src/main/java/bunkBeds/BunkBeds.java
+++ src/main/java/bunkBeds/BunkBeds.java
@@ -1,17 +1,25 @@
 package bunkBeds;
 
+import necesse.engine.localization.Localization;
 import necesse.engine.modLoader.annotations.ModEntry;
 import necesse.engine.modLoader.annotations.ModMethodPatch;
-import necesse.level.maps.levelData.settlementData.SettlementRoom;
+import necesse.entity.mobs.friendly.human.HappinessModifier;
+import necesse.entity.mobs.friendly.human.HumanMob;
 import net.bytebuddy.asm.Advice;
+
+import java.util.List;
+import java.util.function.Predicate;
 
 @ModEntry
 public class BunkBeds {
-    @ModMethodPatch(target = SettlementRoom.class, name = "getOccupiedBeds", arguments = {})
-    public static class GetOccupiedBedsPatch {
+    public static final Predicate<HappinessModifier> isSharedRoomModifier = (modifier) ->
+        modifier.description.translate().startsWith(Localization.translate("settlement", "sharingroom"));
+
+    @ModMethodPatch(target = HumanMob.class, name = "getHappinessModifiers", arguments = {})
+    public static class GetHappinessModifiersPatch {
         @Advice.OnMethodExit
-        public static void onExit(@Advice.Return(readOnly = false) int occupiedBeds) {
-            occupiedBeds = Math.min(occupiedBeds, 1);
+        public static void onExit(@Advice.Return(readOnly = false) List<HappinessModifier> modifiers) {
+            modifiers.removeIf(isSharedRoomModifier);
         }
     }
 }
